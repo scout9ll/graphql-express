79e0c3c8de175cb205d361d29344778abc1c5920
steve9ll <qiuliaolin1@163.com>
2019-12-05 00:03:59 +0800

    feat : build first graphq api

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,39 @@
 import express, { Application } from "express";
 import bodyParser from "body-parser";
 import graphqlHttp from "express-graphql";
-
+import { buildSchema } from "graphql";
 const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(
   "/graphql",
   graphqlHttp({
-    schema: null,
-    rootValue: {}
+    schema: buildSchema(
+      `
+      type RootQuery{
+        events:[String!]!
+      }
+      type RootMutation {
+        createEvent(name:String):String
+      }
+
+      schema{
+        query:RootQuery
+        mutation:RootMutation
+      }
+      `
+    ),
+
+    rootValue: {
+      events: () => {
+        return ["baby driver"];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
   })
 );
 app.get("/", (req, res, next) => {
