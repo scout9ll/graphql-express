e3cf9287a5a74af6bb0ddef07e97f9a2f196332b
scout <scout@911.com>
2020-02-24 22:12:18 +0800

    feat: add mongo db

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,10 @@ import express, { Application } from "express";
 import bodyParser from "body-parser";
 import graphqlHttp from "express-graphql";
 import { buildSchema } from "graphql";
+import mongoose from "mongoose";
+import Event from './models/event'
 const app: Application = express();
 
-const events = [];
 app.use(bodyParser.json());
 app.use(
   "/graphql",
@@ -40,25 +41,51 @@ app.use(
 
     rootValue: {
       events: () => {
-        return events;
+        return Event.find().then(
+          events => {
+            return events.map(event => {
+              return event
+            }
+            )
+          }
+        ).catch(err => console.log(err)
+        );
       },
       createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
-          title: args.eventInput.description,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
-        events.push(event);
-        return event;
+        const event = new Event(
+          {
+            title: args.eventInput.description,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: args.eventInput.date
+          }
+        )
+        return event.save().then(
+          result => {
+            console.log(result);
+            return result
+          }
+        ).catch(
+          err => {
+            throw err;
+          }
+        )
       }
     },
     graphiql: true
   })
 );
+
+mongoose.connect(
+  `mongodb+srv://test:87955626@music-6epxh.mongodb.net/ge_test?retryWrites=true&w=majority`
+).then(() => {
+  app.listen(3000);
+}).catch(err => {
+  console.log(err);
+
+})
+
 app.get("/", (req, res, next) => {
   res.send("hello,world");
 });
 
-app.listen(3000);
